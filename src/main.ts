e11c6528bc5c0393b40ff134c0adf6f0cd0f23fd
Leul Yohannes <106162490+ltyohannes955@users.noreply.github.com>
2026-06-03 14:44:11 +0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
   app.useStaticAssets(join(process.cwd(), 'uploads'), {
